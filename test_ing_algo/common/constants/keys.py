4d28ea90d4e88a84b483062e297883f796c36228
AlvaroRDP.pr <alvarordp.pr@gmail.com>
2025-03-23 18:00:12 +0100

    feat: add "keys" folder and fill with csv heads

---
+++ test_ing_algo/common/constants/keys.py
@@ -0,0 +1,13 @@
+"""Keys folder.
+
+Mainly used to automate string completion and avoid spelling issues.
+"""
+
+TIME = "time"
+HEART_RATE = "heart_rate"
+CNFS = "cnfs"
+ANY_MOTION = "any_motion"
+HIGH_G = "high_g"
+X = "x"
+Y = "y"
+ONSKIN = "onskin"
